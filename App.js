0ef78d02eb6eed547707e908d762d29ca29379e1
okunlade Jolade <jokunlade@gmail.com>
2022-05-12 16:31:39 +0100

    adding functionality

---
+++ App.js
@@ -0,0 +1,18 @@
+const game = ()=> {
+    let pScore = 0;
+    let cScore = 0;
+   
+const startGame = ()=> {
+    const playBtn = document.querySelector('.ntro button');
+    const introScreen = document.querySelector('.intro');
+    const match = document.querySelector('.match');
+
+    playBtn.addEventListener('click', () =>{
+introScreen.classList.add('fadeOut');
+    });
+}
+
+
+
+
+}
